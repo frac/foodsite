be262a04913c912264a42a90cfba0b1f6d172aa3
adriano petrich <petrich@gmail.com>
2009-09-29 14:38:21 -0300

    some sorting of the tags

--- middleware/categories.py
+++ middleware/categories.py
@@ -2,6 +2,7 @@ from tagging.models import Tag
 from foodsite.core.models import Post
 import datetime
 from django.core.cache import cache
+import operator
 
 def get_categories(request):
     tags = cache.get('categories')
@@ -9,6 +10,8 @@ def get_categories(request):
     if not tags:
         filter = dict(published_at__isnull=False,published_at__lte=datetime.datetime.today())
         tags = Tag.objects.usage_for_model(Post, counts=True, min_count=0, filters=filter)
+        tags.sort(key=operator.attrgetter("count"), reverse=True)
+        tags = tags[:10]
         cache.set('categories', tags)
 
 
