00de8c2517ca61a6636592f0b052a66c4a1598bc
adriano petrich <petrich@gmail.com>
2009-09-30 20:36:11 -0300

    err missed one

--- core/admin.py
+++ core/admin.py
@@ -61,7 +61,7 @@ class IngredientAdmin(admin.ModelAdmin):
 
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title','image','full_url')
+    list_display = ('title','used','image','full_url')
     actions = [use_photo]
 
 class UnitAdmin(admin.ModelAdmin):
