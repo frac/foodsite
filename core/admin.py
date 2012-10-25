0229ffc73f716354d5315f3be542567070bef0d7
adriano petrich <petrich@gmail.com>
2012-10-25 16:18:07 -0200

    pep8ing

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,7 @@ from models import Recipe, Ingredient, Unit
 import datetime
 from django_push.publisher import ping_hub
 
+
 class MeasurementInline(admin.TabularInline):
     model = Measurement
     extra = 10
@@ -13,7 +14,7 @@ class MeasurementInline(admin.TabularInline):
 def publish(modeladmin, request, queryset):
     for obj in queryset:
         if not obj.published_at:
-            obj.published_at=datetime.datetime.today()
+            obj.published_at = datetime.datetime.today()
             obj.save()
     ping_hub('http://sfp.adrianopetrich.com/feeds/atom/')
 
@@ -21,13 +22,13 @@ def publish(modeladmin, request, queryset):
 def use_photo(modeladmin, request, queryset):
     for obj in queryset:
         if not obj.used:
-            obj.used=True
+            obj.used = True
             obj.save()
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title','pic','slug','published_at')
+    list_display = ('title', 'pic', 'slug', 'published_at')
     ordering = ('-published_at',)
     actions = [publish]
     change_form_template = "admin/change_view_form.html"
@@ -41,11 +42,12 @@ class PostAdmin(admin.ModelAdmin):
             return db_field.formfield(**kwargs)
         return super(PostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
+
 class RecipeAdmin(PostAdmin):
     inlines = [MeasurementInline]
-    list_display = ('title', 'wave','pic','slug', 'tags','published_at')
+    list_display = ('title', 'wave', 'pic', 'slug', 'tags', 'published_at')
 
-    # have to apear again because of the return super   
+    # have to apear again because of the return super
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         """
         Show only pictures that have not been used or that belongs to posts
@@ -58,22 +60,22 @@ class RecipeAdmin(PostAdmin):
 
 class IngredientAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title','pic',)
+    list_display = ('title', 'pic',)
     ordering = ('pic',)
     change_form_template = "admin/change_view_form.html"
 
 
-
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title','used','full_url', 'wave')
+    list_display = ('title', 'used', 'full_url', 'wave')
     list_filter = ('used',)
     actions = [use_photo]
 
+
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('metric','imperial','conversion')
+    list_display = ('metric', 'imperial', 'conversion')
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Recipe,RecipeAdmin)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Unit, UnitAdmin)
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
